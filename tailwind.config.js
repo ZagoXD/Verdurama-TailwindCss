3040cc969137baa6ba5a35bfa6f47e6525f431e7
ZagoXD <128556675+ZagoXD@users.noreply.github.com>
2024-09-02 12:50:53 -0300

    inicio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*.html'], // Isso garante que o Tailwind escaneie seu HTML
+  theme: {
+    extend: {
+      colors: {
+        bgreen: '#5db55b',
+        navgreen: '#006A42',
+        navhover: '#02472d',
+        b2: '#1C682B',
+        b1: '#006A42',
+      },
+    },
+  },
+  plugins: [],
+}
